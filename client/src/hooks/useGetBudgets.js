b525eda27a45487fce60bd9d66356f1ecec747d8
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-03-29 14:06:58 +0530

    improved error handling in useFetchBudgetCategories

--- client/src/hooks/useGetBudgets.js
+++ client/src/hooks/useGetBudgets.js
@@ -2,14 +2,22 @@ import { axiosConfig } from "../utils/axios/axiosConfig";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { addBudgets } from "../utils/slices/budgetSlice";
+import { logout } from "../utils/slices/authSlice";
 
 export const useFetchBudgetCategories = () => {
   const dispatch = useDispatch();
 
   const fetchBudgets = async () => {
-    const res = await axiosConfig.get("/api/v1/users/budget-categories");
-    dispatch(addBudgets(res?.data?.data));
-  };
+    try{
+      const res = await axiosConfig.get("/api/v1/users/budget-categories");
+      dispatch(addBudgets(res?.data?.data));
+    } catch(error){
+      console.log("error: ", error)
+      if(error?.response?.data?.statusCode === 401){
+        dispatch(logout());
+      }
+    }
+  }
 
   useEffect(() => {
     fetchBudgets();
