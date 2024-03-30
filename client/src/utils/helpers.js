735c88e9513bee0efe80786d30ad7afc17fcda21
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-03-30 19:06:51 +0530

    functions dateToString and getIndianLocalTime added

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,5 +1,5 @@
 export const getDoughnutData = (data) => {
-  const { labelsArr, datasetLabel, datasetBgColorsArr, dataArr} = data;
+  const { labelsArr, datasetLabel, datasetBgColorsArr, dataArr } = data;
 
   if (!labelsArr) return new Error("Labels are missing");
   if (!datasetLabel) return new Error("Datasets label is missing");
@@ -19,4 +19,31 @@ export const getDoughnutData = (data) => {
       },
     ],
   };
+};
+
+export const dateToString = (dateVal) => {
+  const date = new Date(dateVal);
+
+  const year = date.getFullYear();
+  const month = date.getMonth()+1;
+  const day = date.getDate();
+  const formattedDate =  day+"-"+month+"-"+year;
+  return formattedDate;
+};
+
+export const getIndianLocalTime = (date) => {
+  const dateObj = new Date(date)
+  const options = {
+    timeZone: "Asia/Kolkata", // Setting time zone to Indian Standard Time
+    hour12: true, // Whether to use 12-hour clock format
+    hour: "numeric",
+    minute: "numeric", 
+    second: "numeric", 
+  };
+
+  // Get the local Indian time string
+  const indianTime = dateObj.toLocaleTimeString("en-IN", options);
+
+  console.log("Indian Time:", indianTime);
+  return indianTime;
 };
